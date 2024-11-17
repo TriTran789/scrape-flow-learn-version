1b6bb9572f8b12a5bfbdcc957dfd9caf2929b687
ThanhTris <thanhtritran8@gmail.com>
2024-11-17 23:34:41 +0700

    test sqlite

--- prisma/migrations/20241117163337_init_db/migration.sql
+++ prisma/migrations/20241117163337_init_db/migration.sql
@@ -1,15 +1,13 @@
 -- CreateTable
 CREATE TABLE "Workflow" (
-    "id" TEXT NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "userId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT,
     "definition" TEXT NOT NULL,
     "status" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Workflow_pkey" PRIMARY KEY ("id")
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
 );
 
 -- CreateIndex
